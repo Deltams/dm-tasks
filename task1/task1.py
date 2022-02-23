be2f8a29349a2093d43e2c47ff823e7f1378712f
Дмитрий <83587062+Deltams@users.noreply.github.com>
2022-02-24 07:11:41 +0800

    Update task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -1,11 +1,23 @@
-n = int(input('Введите n - '))
+while True:
+    n = input('Введите n - ')
+    n = n.replace(' ', '').lower()
+    check = False
+    for i in n:
+        if '0' <= i <= '9':
+            continue
+        print("Ошибка при вводе данных!")
+        check = True
+        break
+    if check:
+        continue
+    break
 # print('Число всех вариантов - ', 2**2**n)
 # print('Число всех цифр - ', 2**n)
+n = int(n)
+var_ch = 2**n
 if n >= 0:
     for i in range(0, 2**2**n):
         t = str(bin(i)[2:])
-        print(
-            (2**n-len(t))*'0' + t
-        )
+        print((var_ch-len(t))*'0' + t)
 else:
     print('error')
